f4e595af365e66078fd8c3022539942bc8cc1ea7
Subham Adhikari <165596073+Subham-Adhikari@users.noreply.github.com>
2026-02-14 19:20:33 +0530

    Update 12_revenue_contribution_.sql

--- scripts/02_ada-scripts/12_revenue_contribution_.sql
+++ scripts/02_ada-scripts/12_revenue_contribution_.sql
@@ -31,3 +31,34 @@ FROM top_ten_percCustomers
 WHERE top_tenCust_percentrank <= 0.1;
 GO
 
+
+
+
+
+--Which product category had highest revenue = ?, CALCULATE THIS WHERE ORDER DATE IS PRESENT.
+--  bike, almost 96.46% of total revenue
+
+SELECT *,
+	SUM(sales_amount) OVER() golbal_revenue,
+	ROUND(
+		CAST(sales_amount AS float) / SUM(sales_amount) OVER() * 100
+		, 2) percContribution_ofCategories
+FROM
+(
+	SELECT
+		P.category,
+		SUM(F.sales_amount) sales_amount
+	FROM gold.fact_sales F
+	INNER JOIN gold.dim_products P
+	ON F.product_key = P.product_key
+	WHERE F.order_date IS NOT NULL
+	GROUP BY P.category
+)T
+ORDER BY sales_amount DESC;
+GO
+
+
+
+
+
+
